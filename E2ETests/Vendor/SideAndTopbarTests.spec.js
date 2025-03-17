e4f7ed5d7ac9e7e7201731866484a7c84f2808eb
joelandrews614 <joelandrews614@gmail.com>
2025-03-17 17:00:19 +0530

    Implemented Vendor's SideAndTopBarTests and Page

--- E2ETests/Vendor/SideAndTopbarTests.spec.js
+++ E2ETests/Vendor/SideAndTopbarTests.spec.js
@@ -4,7 +4,8 @@ import { VendorLoginPage } from "../../pages/Vendor/LoginPage";
 import { SideNavigationPage } from "../../pages/Vendor/SideAndTopbarPage";
 
 let vLoginPage, sideNavigationPage;
-let username = "rajkumarbg_third_iteration_test";
+let username = "joel_the_03";
+let candidateName = "CandidateSixSix";
 
 test.beforeEach(async ({page}) => {
 
@@ -20,19 +21,79 @@ test.beforeEach(async ({page}) => {
 test("Validate the Vendor - Side Navigation's Dashboard", async () => {
 
     await expect(sideNavigationPage.dashboardButton).toBeVisible();
-
-    await validateSideAndTopBar(username, "Dashboard");
-
+    
+    await validateOptionalScenarios(username, "Dashboard");
+    
 });
 
 test("Validate the Vendor - Side Navigation's Candidate Details", async () => {
 
+    await expect(sideNavigationPage.candidateDetailsButton).toBeVisible();
     await sideNavigationPage.clickCandidateDetailsButton();
 
-    await expect(sideNavigationPage.candidateDetailsButton).toBeVisible();
+    await validateOptionalScenarios(username, "Candidate Details");
+});
 
-    await validateSideAndTopBar(username, "Candidate Details");
+test("Validate the Vendor - Side Navigation's Pack Details", async () => {
+   
+    await expect(sideNavigationPage.packDetailsButton).toBeVisible();
+    await sideNavigationPage.clickPackDetailsButton();
 
+    await validateOptionalScenarios(username, "Pack Details");
+});
+
+test("Validate the Vendor - Side Navigation's Insufficiency Details", async () => {
+    
+    await expect(sideNavigationPage.insufficiencyDetailsButton).toBeVisible();
+    await sideNavigationPage.clickInsufficiencyDetailsButton();
+
+    await validateOptionalScenarios(username, "Insufficiency Details");
+})
+
+test("Validate the Vendor - Side Navigation's Additional Cost", async () => {
+    
+    await expect(sideNavigationPage.additionalCostButton).toBeVisible();
+    await sideNavigationPage.clickAdditionalCostButton();
+
+    await validateOptionalScenarios(username, "Additional Cost List");
+});
+
+test("Validate the Vendor - Side Navigation's Billing Details", async () => {
+
+    await expect(sideNavigationPage.billingDetailsButton).toBeVisible();
+    await sideNavigationPage.clickBillingDetailsButton();
+
+    await validateOptionalScenarios(username, "Billing Details");
+});
+
+test("Validate the Vendor - Side Navigation's Status", async () => {
+
+    await expect(sideNavigationPage.statusButton).toBeVisible();
+    await sideNavigationPage.clickStatusButton();
+
+    await validateOptionalScenarios(username, "Status");
+});
+
+test("Validate the Vendor - Side Navigation's User Management", async () => {
+
+    await expect(sideNavigationPage.userManagementButton).toBeVisible();
+    await sideNavigationPage.clickUserManagementButton();
+
+    await validateOptionalScenarios(username, "User Management");
+});
+
+test("Validate the Vendor - Side Navigation's Account Settings", async () => {
+    await expect(sideNavigationPage.accountSettingsButton).toBeVisible();
+    await sideNavigationPage.clickAccountSettingsButton();
+
+    await validateOptionalScenarios(username, "Account Settings");    
+});
+
+test("Validate the Vendor - Side Navigation's Contact Us", async () => {
+    await expect(sideNavigationPage.contactUsButton).toBeVisible();
+    await sideNavigationPage.clickContactUsButton();
+
+    await validateOptionalScenarios(username, "Contact Us");
 });
 
 test.afterEach(async () => {
@@ -42,13 +103,14 @@ test.afterEach(async () => {
 });
 
 // Helper Functions:
-async function validateSideAndTopBar(username , pageTitle){
+async function validateOptionalScenarios(username , pageTitle){
+    // Combined validateSearchBar() and validateSideAndTopBar() in one function to reduce code lines
     await expect(sideNavigationPage.vendorUsername).toHaveText(username);
     await expect(sideNavigationPage.pageTitle).toHaveText(pageTitle);
-    
     await expect(sideNavigationPage.searchBar).toBeVisible();
     await expect(sideNavigationPage.searchBarButton).toBeVisible();
-    
     await expect(sideNavigationPage.notificationButton).toBeVisible();
-}
 
+    await sideNavigationPage.searchCandidate(candidateName);
+    await expect(sideNavigationPage.searchedCandidate).toHaveText(candidateName);
+}
--- pages/Vendor/SideAndTopbarPage.js
+++ pages/Vendor/SideAndTopbarPage.js
@@ -11,6 +11,15 @@ exports.SideNavigationPage = class SideNavigationPage{
         // Side Bar - Middle Section
         this.dashboardButton = page.getByRole("link", {name: "Dashboard"});
         this.candidateDetailsButton = page.getByRole("link", {name: "Candidate Details"});
+        this.packDetailsButton = page.getByRole("link", {name: "Pack Details"});
+        this.insufficiencyDetailsButton = page.getByRole("link", {name: "Insufficiency"});
+        this.additionalCostButton = page.getByRole("link", {name: "Additional Cost"});
+        this.billingDetailsButton = page.getByRole("link", {name: "Billing Details"});
+        this.statusButton = page.getByRole("link", {name: "Status"});
+        this.userManagementButton = page.getByRole("link", {name: "User Management"});
+        this.accountSettingsButton = page.getByRole("link", {name: "Account Settings"});
+        this.contactUsButton = page.getByRole("link", {name: "Contact Us"});
+
 
         // Side Bar - Bottom Section
         // LogOut Button
@@ -24,6 +33,10 @@ exports.SideNavigationPage = class SideNavigationPage{
         this.searchBarButton = page.locator("//div[@class='searchBar']//button");
         this.notificationButton = page.locator("//div[@class='notificationSvg']");
         this.notificationTitle = page.locator("//header[@class='NotificationHeader']/h1");
+        
+        // Search Candidate List
+        this.searchedCandidate = page.locator("//td[@data-index='1']/span/span");
+
     };
 
     // Side Bar - Middle Section
@@ -34,6 +47,38 @@ exports.SideNavigationPage = class SideNavigationPage{
     async clickCandidateDetailsButton(){
         await this.candidateDetailsButton.click();
     };
+
+    async clickPackDetailsButton(){
+        await this.packDetailsButton.click();
+    };
+
+    async clickInsufficiencyDetailsButton(){
+        await this.insufficiencyDetailsButton.click();
+    };
+
+    async clickAdditionalCostButton(){
+        await this.additionalCostButton.click();
+    }
+
+    async clickBillingDetailsButton(){
+        await this.billingDetailsButton.click();
+    }
+
+    async clickStatusButton(){
+        await this.statusButton.click();
+    }
+
+    async clickUserManagementButton(){
+        await this.userManagementButton.click();
+    }
+
+    async clickAccountSettingsButton(){
+        await this.accountSettingsButton.click();
+    }
+
+    async clickContactUsButton(){
+        await this.contactUsButton.click();
+    }
 
     // Side Bar - Bottom Section
     async logout(){
